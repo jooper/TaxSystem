50961a295fb160bf0df1b576ffb3a444aa2a2c14
蒋维平 <jiangweiping@dfwd.com>
2018-07-20 17:38:35 +0800

    develop branche

--- Tax.CompanyModuleService/UnitOfWork/EFUnitOfWork.cs
+++ Tax.CompanyModuleService/UnitOfWork/EFUnitOfWork.cs
@@ -1,11 +1,13 @@
 ﻿using System.Data.Entity;
 using Tax.ICompanyModuleService.Domain.BaseModel;
+using Tax.ICompanyModuleService.Domain.BaseModel.Models;
 
 namespace Tax.CompanyModuleService.UnitOfWork
 {
-    public class EFUnitOfWork:IEFUnitOfWork
+    public class EFUnitOfWork : IEFUnitOfWork
     {
         public bool IsCommitted { get; set; }
+
         public int Commit()
         {
             throw new System.NotImplementedException();
@@ -36,6 +38,19 @@ namespace Tax.CompanyModuleService.UnitOfWork
             throw new System.NotImplementedException();
         }
 
-        public DbContext Context { get; }
+        public DbContext Context => new EFDbContext();
+    }
+
+
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base("name=")
+        {
+        }
+        public DbSet<TbCompany> TB_COMPANY { set; get; }
+        public DbSet<TbRole> TB_ROLE { set; get; }
+        public DbSet<TbRight> TB_RIGHT { set; get; }
+        public DbSet<TbUserrole> TB_USERROLE { set; get; }
+        public DbSet<TbUser> TB_USER { set; get; }
     }
 }
--- Tax.CompanyModuleService/UnitOfWork/IEFUnitOfWork.cs
+++ Tax.CompanyModuleService/UnitOfWork/IEFUnitOfWork.cs
@@ -1,24 +1,10 @@
-﻿using Tax.ICompanyModuleService.Domain.IRepositories;
-using System.Data.Entity;
-using Tax.ICompanyModuleService.Domain.BaseModel.Models;
+﻿using System.Data.Entity;
+using Tax.ICompanyModuleService.Domain.IRepositories;
 
 namespace Tax.CompanyModuleService.UnitOfWork
 {
     public interface IEFUnitOfWork : IUnitOfWorkRespositoryContext
     {
         DbContext Context { get; }
-    }
-
-
-    public class EFDbContext : DbContext
-    {
-        public EFDbContext() : base("name=")
-        {
-        }
-        public DbSet<TbCompany> TB_COMPANY { set; get; }
-        public DbSet<TbRole> TB_ROLE { set; get; }
-        public DbSet<TbRight> TB_RIGHT { set; get; }
-        public DbSet<TbUserrole> TB_USERROLE { set; get; }
-        public DbSet<TbUser> TB_USER { set; get; }
     }
 }
