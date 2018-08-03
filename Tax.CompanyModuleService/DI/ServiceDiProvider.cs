a3ac5f112cf7a433d499e768a0d479ea4be7412b
蒋维平 <jiangweiping@dfwd.com>
2018-08-03 13:20:05 +0800

    cleanup code

--- Tax.CompanyModuleService/DI/ServiceDiProvider.cs
+++ Tax.CompanyModuleService/DI/ServiceDiProvider.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Tax.CompanyModuleService.UnitOfWork;
 
 namespace Tax.CompanyModuleService.DI
 {
+    //Lib内部依赖注入模块
     public class ServiceDiProvider
     {
         private static IServiceCollection _serviceCollection;
+
         private static readonly object Lock = new object();
+
+        public static ServiceProvider GetDiProivder()
+        {
+//            var efUnitOfWork = serviceProvider.GetService<IEfUnitOfWork>();
+            return SingleServiceCollection().BuildServiceProvider();
+        }
 
         private static IServiceCollection SingleServiceCollection()
         {
@@ -26,23 +33,13 @@ namespace Tax.CompanyModuleService.DI
 
         private static void DiContext()
         {
+            //迁移到 EF CORE内部去完成连接串的配置
 //            _serviceCollection.AddEntityFrameworkSqlServer()
 //                .AddDbContext<EfDbContext>(options => { options.UseSqlServer(DefaultSqlConnectionString); },
 //                    ServiceLifetime.Scoped);
             _serviceCollection.AddEntityFrameworkSqlServer().AddDbContext<EfDbContext>(ServiceLifetime.Scoped);
             _serviceCollection.AddTransient<IEfUnitOfWork, EfUnitOfWork>();
             _serviceCollection.AddTransient<IEfDbContext, EfDbContext>();
-//            var serviceProvider = _serviceCollection.BuildServiceProvider();
-//                        var efUnitOfWork = serviceProvider.GetService<IEfUnitOfWork>();
-
-
- 
-        }
-
-
-        public static ServiceProvider GetDiProivder()
-        {
-            return SingleServiceCollection().BuildServiceProvider();
         }
     }
 }
