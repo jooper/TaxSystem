b62f3c3ff918e4ec7a0d1e280a4b6d6570e74856
蒋维平 <jiangweiping@dfwd.com>
2018-08-15 10:10:45 +0800

    账户模块调通

--- Tax.CompanyModuleService/Services/BankAccountService.cs
+++ Tax.CompanyModuleService/Services/BankAccountService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Surging.Core.ProxyGenerator;
@@ -6,22 +7,54 @@ using Tax.CompanyModuleService.Domain.Respositories;
 using Tax.CompanyModuleService.Ext;
 using Tax.ICompanyModuleService.Domain.BaseModel.DTO;
 using Tax.ICompanyModuleService.Domain.BaseModel.Entities;
+using Tax.ICompanyModuleService.Domain.IRepositories;
 using Tax.ICompanyModuleService.Services;
 
 namespace Tax.CompanyModuleService.Services
 {
-    public class BankAccountService : ProxyServiceBase, IBankAccountSevice
+    public class BankAccountService : ProxyServiceBase, IBankAccountService
     {
-        private readonly BankAccountRespository _bankAccountRespository;
+        private readonly IBankAccountRespository _bankAccountRespository;
 
         public BankAccountService(BankAccountRespository bankAccountRespository)
         {
             _bankAccountRespository = bankAccountRespository;
         }
 
-        public async Task<List<BankAccount>> GetBandAccountsAsync()
+        public async Task<int> GetCountAsync()
         {
-            return await _bankAccountRespository.Entities.ToListAsync();
+            var count = _bankAccountRespository.Entities.Count(w => w.IsValied);
+            return await Task.FromResult(count);
+        }
+
+        public async Task<List<BankAccount>> GetBandAccountsAsync(int offSet, int take)
+        {
+            var bankEntities = _bankAccountRespository.Entities.OrderByDescending(o => o.UpdateTime)
+                .Where(w => w.IsValied)
+                .GroupBy(g => g.CompayId).Select(x => new
+                {
+                    CompanyId = x.Key,
+                    Account = x.Select(w => new BankAccount
+                    {
+                        CompayId = w.CompayId,
+                        OpenBankName = w.OpenBankName,
+                        CompanyName = w.CompanyName,
+                        AccountType = w.AccountType,
+                        AccountNumber = w.AccountNumber,
+                        LinkMan = w.LinkMan,
+                        LinkManPhone = w.LinkManPhone,
+                        State = w.State,
+                        SealExplation = w.SealExplation
+                    })
+                });
+
+            var ddd = await bankEntities.Skip(offSet).Take(take).ToListAsync();
+
+
+            var companies = await _bankAccountRespository.Entities.Where(w => w.IsValied)
+                .OrderByDescending(o => o.UpdateTime).Skip(offSet).Take(take)
+                .ToListAsync();
+            return companies;
         }
 
         public async Task<int> AddBankAccountAsync(DBankAccount bankAccount)
--- Tax.ICompanyModuleService/Services/IBankAccountService.cs
+++ Tax.ICompanyModuleService/Services/IBankAccountService.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Surging.Core.CPlatform.Ioc;
+using Surging.Core.CPlatform.Runtime.Server.Implementation.ServiceDiscovery.Attributes;
 using Tax.ICompanyModuleService.Domain.BaseModel.DTO;
 using Tax.ICompanyModuleService.Domain.BaseModel.Entities;
 
 namespace Tax.ICompanyModuleService.Services
 {
-    public interface IBankAccountSevice : IServiceKey
+    [ServiceBundle("api/{Service}")]
+    public interface IBankAccountService : IServiceKey
     {
-        Task<List<BankAccount>> GetBandAccountsAsync();
+        Task<int> GetCountAsync();
+        Task<List<BankAccount>> GetBandAccountsAsync(int offSet, int take);
         Task<int> AddBankAccountAsync(DBankAccount bankAccount);
         Task<int> UpdateBankAccountAsync(DBankAccount bankAccount);
-
     }
 }
