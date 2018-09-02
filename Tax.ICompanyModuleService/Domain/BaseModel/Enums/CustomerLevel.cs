34445ae3d88fbc6281d8a56e58978b855bcf9081
蒋维平 <jiangweiping@dfwd.com>
2018-09-02 20:04:02 +0800

    完成基本功能

--- Tax.ICompanyModuleService/Domain/BaseModel/Enums/CustomerLevel.cs
+++ Tax.ICompanyModuleService/Domain/BaseModel/Enums/CustomerLevel.cs
@@ -3,8 +3,14 @@
     public enum CustomerLevel
     {
         //todo
-        Common, //普通
-        Perfsenior, //高级
-        Vip
+//        Common, //普通
+//        Perfsenior, //高级
+//        Vip
+        //  0，1，2，3，4
+        OneStar,//1星
+        TwoStar,
+        ThreeStar,
+        FourStar,
+        FridyStar
     }
 }
